c003d34594aa2f6c89b17962e1b155c15cf7da99
we-z <yousefsalem00@gmail.com>
2026-08-10 08:29:38 -0700

    Reduce site to standalone index.html

--- worker/http.js
+++
@@ -1,9 +0,0 @@
-export function redirect(location, status = 308) {
-  return new Response(null, {
-    status,
-    headers: {
-      Location: location,
-      'Cache-Control': 'private, no-store, max-age=0',
-    },
-  });
-}
